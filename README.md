b54ac08ad178b78df959b32167b10832e8635191
Graceatkirk <grace@kirk.org>
2024-08-12 20:14:32 -0500

    upadtes to lots of stuff

--- README.md
+++ README.md
@@ -1,22 +1,30 @@
 # Hist-O-Match
-A History Matching Game!
+## History Matching Game
+ Looking at the six cards on the screen, the player must type the cards' corresponding letters into the input boxes at the bottom of the page. Enter the cards in the order that the events occured thoughout history!
+ These topics can vary from US History to World History, be sure that you have your thinking cap on!
 
-## Description
+ ![This is the screen that shows the game in action](./assets/pics/ReadME/GameScreenshot.jpg)
 
-## How to Play
-Looking at the six cards on the screen, the player must type the cards' corresponding letters into the input boxes at the bottom of the page. Enter the cards in the order that the events occured thoughout history! 
+ ## Results
+When you press the 'check' button after entering your answers, you will be shown what answers you got correct and/or incorrect. You can repeat the game until you have the order logged correctly. When everything is right, the actual dates of the events will show up on the cards. If you'd like to play again, hit the "Play Again" button
 
-## Results
-When you press the 'check' button after entering your answers, you will be shown what answers you got correct and/or incorrect. You can repeat the game until you have the order logged correctly. When everything is right, the actual dates of the events will show up on the cards.
+![This is a screenshot of the end game (modified to remove the answers)](./assets/pics/ReadME/EndGame.jpg)
 
-## Licence
-MIT Licence
+## High Scores
+If you would like to see your High Score, inspect your webpage and head over to Application. Here it will show you your top 3 scores!
+
+![This shows you my High Scores!](./assets/pics/ReadME/High-Scores.jpg)
+
+
+## License
+MIT License
 
 ## Credits
-Created and published by Andrew Angus, Dario Zambrano and Grace Kirk.
+Created and published by Andrew Angus, Dario Zambrano, and Grace Kirk
 
 ## Contact Information
-Check us out at github! 
-Andrew = https://github.com/aangus98
-Dario = https://github.com/DarioJZB
-Grace = https://github.com/Graceatkirk
+Check us out on Github!
+ 
+ * Andrew:  https://github.com/aangus98 
+ * Dario: https://github.com/DarioJZB
+ * Grace: https://github.com/Graceatkirk
--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,29 @@
-const correctOrder = ['A', 'B', 'C', 'D', 'E', 'F'];
+const correctOrder = ['D', 'F', 'B', 'A', 'C', 'E'];
 
 const checkButton = document.getElementById('checkButton');
 const modal = document.getElementById('myModal');
 const closeBtn = document.getElementsByClassName('close')[0];
 const correctEventsList = document.getElementById('correctEvents');
 const incorrectEventsList = document.getElementById('incorrectEvents');
-
+const cards = document.getElementsByClassName('card');
 const year = document.getElementsByClassName('year');
+const timer = document.getElementById('timer');
+const startButton = document.getElementById('startButton');
+const yourTime = document.getElementById('yourTime');
+const playAgain = document.getElementById('playAgain');
 
+let time, timerInterval;
 
-console.log(correctOrder.join(' '));
-console.log(correctOrder);
-
+startButton.addEventListener('click', startTimer);
 
 checkButton.addEventListener('click', function(event) {
     event.preventDefault();
-    let userOrder = Array.from(document.querySelectorAll('#answer-container input')).map(input => input.value.toUpperCase()).join(' ');
- console.log(userOrder)   
+    let userOrder = Array.from(document.querySelectorAll('#answer-container input')).map(input => input.value.toUpperCase()).join(' '); 
     if (userOrder === correctOrder.join(' ')) {
-        correctEventsList.innerHTML = '';
-        incorrectEventsList.innerHTML = '';
-        for (let i = 0; i < year.length; i++) {
-            year[i].style.display = 'flex';
-        }
-        correctOrder.forEach(event => {
-            const li = document.createElement('li');
-            li.textContent = getEventName(event);
-            correctEventsList.appendChild(li);
-        });
+        endGame();
     } else {
-        correctEventsList.innerHTML = '';
-        incorrectEventsList.innerHTML = '';
-        correctOrder.forEach((event, index) => {
-            const li = document.createElement('li');
-            if (event === userOrder.split(' ')[index]) {
-                li.textContent = getEventName(event);
-                correctEventsList.appendChild(li);
-            } else {
-                li.textContent = getEventName(event);
-                incorrectEventsList.appendChild(li);
-            }
-        });
+        showIncorrect(userOrder);
     }
-
-    modal.style.display = 'flex';
 });
 
 closeBtn.addEventListener('click', function() {
@@ -55,22 +35,90 @@ window.addEventListener('click', function(event) {
         modal.style.display = 'none';
     }
 });
-
+function startTimer(event) {
+    event.preventDefault();
+    startModal.style.display = 'none';
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.setProperty('display', 'flex', 'important');
+    }
+    time = 0;
+    timerInterval = setInterval(() => {
+        time++;
+        let seconds = time/100;
+        timer.textContent = "Time: "+ seconds.toFixed(0);
+    }, 10);
+}
 function getEventName(event) {
     switch(event) {
-        case 'A':
-            return 'The Fall of Constantinople';
-        case 'B':
-            return 'American Revolution';
-        case 'C':
-            return 'American Civil War';
         case 'D':
-            return 'World War 1';
-        case 'E':
-            return 'World War 2';
+            return 'The Fall of Constantinople';
         case 'F':
+            return 'American Revolution';
+        case 'B':
+            return 'American Civil War';
+        case 'A':
+            return 'World War 1';
+        case 'C':
+            return 'World War 2';
+        case 'E':
             return 'Fall of the Berlin Wall';
         default:
             return 'Unknown Event';
+    }
+}
+
+function endGame() {
+    clearInterval(timerInterval);
+        correctEventsList.innerHTML = '';
+        incorrectEventsList.innerHTML = '';
+        for (let i = 0; i < year.length; i++) {
+            year[i].style.display = 'flex';
+        }
+        correctOrder.forEach(event => {
+            const li = document.createElement('li');
+            li.textContent = getEventName(event);
+            correctEventsList.appendChild(li);
+        });
+    time = time/100;
+    yourTime.textContent = "Your time: " + time + " seconds";    
+    checkScores();
+    playAgain.addEventListener('click', function() {  
+        location.reload();
+    });
+
+    modal.style.display = 'flex';
+    modal.style.flexDirection = 'column';
+}
+
+function showIncorrect(userOrder) {
+    correctEventsList.innerHTML = '';
+    incorrectEventsList.innerHTML = '';
+    correctOrder.forEach((event, index) => {
+        const li = document.createElement('li');
+        if (event === userOrder.split(' ')[index]) {
+            li.textContent = getEventName(event);
+            correctEventsList.appendChild(li);
+        } else {
+            li.textContent = getEventName(event);
+            incorrectEventsList.appendChild(li);
+        }
+        modal.style.display = 'flex';
+    });  
+      
+}
+
+function checkScores() {
+    let localScores = localStorage.getItem('highScores');
+    if (localScores === null){
+        let scoreArray = [time];
+        localStorage.setItem('highScores', JSON.stringify(scoreArray));
+    } else {
+        let scoreArray = JSON.parse(localScores);
+        scoreArray.push(time);
+        scoreArray.sort();
+        if (scoreArray.length > 3) {
+            scoreArray.pop();
+        }
+        localStorage.setItem('highScores', JSON.stringify(scoreArray));
     }
 }
